1b9b529c2046df77ec3aba2f75d5760c036b10ae
Emirkhan03 <emirkhanismailov03@gmail.com>
2022-12-05 15:02:59 +0600

    Initial commit

---
+++ registration-login-springboot-security-thymeleaf-master/registration-login-springboot-security-thymeleaf-master/src/main/java/com/example/registrationlogindemo/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.registrationlogindemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @Column(name = "name")
+//    private String name;
+
+    @Column(name = "log")
+    private String log;
+
+    @Column(name="Email", unique=true)
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Role> roles = new ArrayList<>();
+
+}
